fd6893ed322b0b36b2d53ab50aa1764d17b96350
rased100 <rased100@gmail.com>
2022-01-14 23:31:06 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { useState } from 'react';
+import initializeAuthentication from './Firebase/firebase.initialize';
+
+initializeAuthentication();
+
+const googleProvider = new GoogleAuthProvider();
 
 function App() {
+  const [user, setUser] = useState({});
+  const auth = getAuth();
+
+  const { name, email, photo } = user;
+
+  const handleGoogleSignIn = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        const { displayName, email, photoURL } = result.user;
+        const loggedInUser = {
+          name: displayName,
+          email: email,
+          photo: photoURL
+        }
+        setUser(loggedInUser);
+      })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+        <button onClick={handleGoogleSignIn}>Google Sign In Button</button>
+        <h2>{name}</h2>
+        <h4>{email}</h4>
+        <img src={photo} alt="" />
+      </div>
     </div>
   );
 }
---
+++ src/Firebase/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDcUImhpFSdLg0HVO8jjsR6_j5ZINDuB0A",
+    authDomain: "fir-all-practice.firebaseapp.com",
+    projectId: "fir-all-practice",
+    storageBucket: "fir-all-practice.appspot.com",
+    messagingSenderId: "146246442899",
+    appId: "1:146246442899:web:1a1f3c5f976ce64d2e1266"
+};
+
+export default firebaseConfig;
